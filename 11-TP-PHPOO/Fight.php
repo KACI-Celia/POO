fcf15a165c23d1c37f3ada17c1a9c36b50accd26
Celia KACI <celiakaci23@yahoo.com>
2023-12-11 09:28:43 +0100

    modification

--- 11-TP-PHPOO/Fight.php
+++ 11-TP-PHPOO/Fight.php
@@ -1,11 +1,20 @@
 <?php
+
 require_once("Ippo.php");
 require_once("Challenger.php");
 
 
 class Fight
 {
-    public function figh()
+    public function fighting($ippo, $challenger)
+    {
+        $round=10;
+        for($round=1; $round<=10; $round++){
+            echo "Round $round . \n";
+        }
+        $staminaIppo = $ippo->stamina - $challenger->strength;
+        $staminaChallenger = $challenger->stamina - $ippo->strength;
+    }
 }
 
 
